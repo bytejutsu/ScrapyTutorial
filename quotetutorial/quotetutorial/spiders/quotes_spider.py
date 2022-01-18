b14dbb531bd1320d894cfe66b8d22dfdaa82dd02
Dhia Nabli <dhianabli@outlook.com>
2022-01-18 15:42:15 +0100

    improve scraped data and store it in a json file

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -9,18 +9,19 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-
         quoteTutorialItem = QuotetutorialItem()
 
         all_div_quotes = response.css('div.quote')
 
         for quote in all_div_quotes:
+
             title = quote.css('span.text::text').extract()
             author = quote.css('.author::text').extract()
-            tag = quote.css('.tag::text').extract()
+            tags = quote.css('.tag::text').extract()
+
 
             quoteTutorialItem['title'] = title
             quoteTutorialItem['author'] = author
-            quoteTutorialItem['tag'] = tag
+            quoteTutorialItem['tags'] = tags
 
-        yield quoteTutorialItem
+            yield quoteTutorialItem
